b4f08cb8b3665c992ff6111dccf8b95213bda374
PhilDez <pdesiletssds@gmail.com>
2019-07-18 20:13:37 -0400

    First commit boys
    - mouvement du player
    - jump + dash
    commentaire : les valeurs des mécaniques sont encore à être déterminées. Ainsi, les contrôles ne feel pas encore soft mais je planifie de le rendre puisque c'est la base du jeu!

---
+++ Le Jeu/Assets/Player/Grounded.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grounded : MonoBehaviour
+{
+    GameObject Player;
+
+    void Start()
+    {
+        Player = gameObject.transform.parent.gameObject;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Ground")
+        {
+            Player.GetComponent<Playermouvement>().isGrounded = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Ground")
+        {
+            Player.GetComponent<Playermouvement>().isGrounded = false;
+        }
+    }
+}
---
+++ Le Jeu/Assets/Player/Playermouvement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playermouvement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+    public float dashForce = 15f;
+    public bool isGrounded = false;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Jump();
+        Dash();
+        Vector3 mouvement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += mouvement * Time.deltaTime * moveSpeed;
+    }
+
+    void Jump()
+    {
+        if(Input.GetButtonDown("Jump") && isGrounded == true)
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+
+    void Dash()
+    {
+        if (Input.GetButtonDown("Dash"))
+        {
+            //gameObject.GetComponent<Rigidbody2D>().gravityScale = 0;
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(dashForce, 0f), ForceMode2D.Impulse);
+        }
+    }
+}
